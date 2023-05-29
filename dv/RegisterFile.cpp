a1a371a5c385f54f2a72a249eb57be869adedafc
Vito Gamberini <vito@gamberini.email>
2023-05-29 04:58:37 -0400

    fix: Don't use global statics in tests

--- dv/RegisterFile.cpp
+++ dv/RegisterFile.cpp
@@ -2,9 +2,7 @@
 #include <random>
 #include <VRegisterFile.h>
 
-static VRegisterFile rf;
-
-static auto tick = [](int ticks = 1) {
+static void tick(VRegisterFile& rf, int ticks = 1) {
   for(int i {0}; i < ticks; ++i) {
     rf.clk = 0;
     rf.eval();
@@ -13,16 +11,17 @@ static auto tick = [](int ticks = 1) {
   }
 };
 
-static auto init = [] {
+static void init(VRegisterFile& rf) {
   rf.nReset = 1;
   rf.eval();
   rf.nReset = 0;
   rf.eval();
   rf.nReset = 1;
-};
+}
 
 TEST_CASE("Register File, Write 0") {
-  init();
+  VRegisterFile rf;
+  init(rf);
 
   rf.srcA = 0;
   rf.srcB = 0;
@@ -30,21 +29,22 @@ TEST_CASE("Register File, Write 0") {
   rf.inData = 1;
   rf.we = 0;
 
-  tick();
+  tick(rf);
 
   REQUIRE(rf.outDataA == 0);
   REQUIRE(rf.outDataB == 0);
 
   rf.we = 1;
 
-  tick(2);
+  tick(rf, 2);
 
   REQUIRE(rf.outDataA == 0);
   REQUIRE(rf.outDataB == 0);
 }
 
 TEST_CASE("Register File, Write/Read with we=1") {
-  init();
+  VRegisterFile rf;
+  init(rf);
 
   rf.srcA = 0;
   rf.srcB = 0;
@@ -54,17 +54,17 @@ TEST_CASE("Register File, Write/Read with we=1") {
   for(int i {1}; i < 32; ++i) {
     rf.dst = i;
     rf.inData = 0;
-    tick();
+    tick(rf);
 
     rf.srcA = i;
     rf.srcB = i;
-    tick();
+    tick(rf);
 
     REQUIRE(rf.outDataA == 0);
     REQUIRE(rf.outDataB == 0);
 
     rf.inData = i;
-    tick(2);
+    tick(rf, 2);
 
     REQUIRE(rf.outDataA == i);
     REQUIRE(rf.outDataB == i);
@@ -73,7 +73,8 @@ TEST_CASE("Register File, Write/Read with we=1") {
 
 
 TEST_CASE("Register File, Write/Read with we=0") {
-  init();
+  VRegisterFile rf;
+  init(rf);
 
   rf.srcA = 0;
   rf.srcB = 0;
@@ -83,7 +84,7 @@ TEST_CASE("Register File, Write/Read with we=0") {
   for(int i {1}; i < 32; ++i) {
     rf.dst = i;
     rf.inData = i;
-    tick();
+    tick(rf);
   }
 
   rf.we = 0;
@@ -91,7 +92,7 @@ TEST_CASE("Register File, Write/Read with we=0") {
   for(int i {0}; i < 32; ++i) {
     rf.srcA = i;
     rf.srcB = i;
-    tick();
+    tick(rf);
 
     REQUIRE(rf.outDataA == i);
     REQUIRE(rf.outDataB == i);
