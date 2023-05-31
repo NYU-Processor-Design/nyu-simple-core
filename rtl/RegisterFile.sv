0e71e15506a3fee810d1f22d3973f346408ba741
Vito Gamberini <vito@gamberini.email>
2023-05-31 04:14:53 -0400

    refactor: RegisterFIle.sv

--- rtl/RegisterFile.sv
+++ rtl/RegisterFile.sv
@@ -1,30 +1,33 @@
-module RegisterFile #(WordSize = 32)
-(
-  input clk,
-  input nReset,
-  input we,
+module RegisterFile #(
+    int WordSize = 32
+) (
+    input clk,
+    input nReset,
+    input we,
 
-  input [4:0] srcA,
-  input [4:0] srcB,
-  input [4:0] dst,
+    input [4:0] srcA,
+    input [4:0] srcB,
+    input [4:0] dst,
 
-  input [WordSize-1:0] inData,
-  output logic [WordSize-1:0] outDataA,
-  output logic [WordSize-1:0] outDataB
+    input [WordSize-1:0] inData,
+    output logic [WordSize-1:0] outDataA,
+    output logic [WordSize-1:0] outDataB
 );
 
+  logic [WordSize-1:0] registers[1:31];
 
-  logic [WordSize-1:0] registers[31];
   always_ff @(posedge clk, negedge nReset) begin
-    if(!nReset)
-      for(int i = 0; i < $size(registers); ++i)
+    // verilog_format: off
+    if (!nReset)
+      for (int i = 0; i < $size(registers); ++i)
         registers[i] <= 0;
 
-    if(we && |dst)
-      registers[dst - 1] <= inData;
+    if (we && |dst)
+      registers[dst] <= inData;
+    // verilog_format: on
 
-    outDataA <= |srcA ? registers[srcA - 1] : 0;
-    outDataB <= |srcB ? registers[srcB - 1] : 0;
+    outDataA <= |srcA ? registers[srcA] : 0;
+    outDataB <= |srcB ? registers[srcB] : 0;
 
   end
 
